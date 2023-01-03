a90a822657c3eaee94b23612690201da8cb7ff39
Manuel Valls <me@manvalls.com>
2023-01-03 23:52:27 +0100

    Implement selectorcache

--- selectorcache/selectorcache.go
+++ selectorcache/selectorcache.go
@@ -1,11 +1,35 @@
 package selectorcache
 
-import "github.com/andybalholm/cascadia"
+import (
+	"fmt"
+
+	"github.com/andybalholm/cascadia"
+)
 
 type SelectorCache struct {
+	cache map[string]cascadia.Sel
+}
+
+func New() *SelectorCache {
+	return &SelectorCache{
+		cache: make(map[string]cascadia.Sel),
+	}
 }
 
 func (s *SelectorCache) Get(selector string, args []interface{}) (cascadia.Sel, error) {
-	// TODO
-	return nil, nil
+	if len(args) > 0 {
+		return cascadia.Parse(fmt.Sprintf(selector, args...))
+	}
+
+	if cached, ok := s.cache[selector]; ok {
+		return cached, nil
+	}
+
+	sel, err := cascadia.Parse(selector)
+	if err != nil {
+		return nil, err
+	}
+
+	s.cache[selector] = sel
+	return sel, nil
 }
