c9d05c06b0307ed637716a8f91d58099d2625905
Manuel Valls <me@manvalls.com>
2023-01-04 12:21:08 +0100

    Add selector and selectorAll

--- htmlsender/transaction.go
+++ htmlsender/transaction.go
@@ -1,16 +1,105 @@
 package htmlsender
 
-import "github.com/manvalls/gosen/commands"
+import (
+	"github.com/andybalholm/cascadia"
+	"github.com/manvalls/gosen/commands"
+	"golang.org/x/net/html"
+)
+
+type SelectedNodes struct {
+	isFragment bool
+	nodes      []*html.Node
+}
+
+func queryFirst(parent *SelectedNodes, selector cascadia.Sel) *SelectedNodes {
+	if parent.isFragment {
+		for _, node := range parent.nodes {
+			if selector.Match(node) {
+				return &SelectedNodes{
+					isFragment: false,
+					nodes:      []*html.Node{node},
+				}
+			}
+		}
+	}
+
+	for _, node := range parent.nodes {
+		result := cascadia.Query(node, selector)
+		if result != nil {
+			return &SelectedNodes{
+				isFragment: false,
+				nodes:      []*html.Node{result},
+			}
+		}
+	}
+
+	return &SelectedNodes{
+		isFragment: false,
+		nodes:      []*html.Node{},
+	}
+}
+
+func queryAll(parent *SelectedNodes, selector cascadia.Sel) *SelectedNodes {
+	nodes := make([]*html.Node, 0)
+
+	if parent.isFragment {
+		for _, node := range parent.nodes {
+			if selector.Match(node) {
+				nodes = append(nodes, node)
+			}
+		}
+	}
+
+	for _, node := range parent.nodes {
+		result := cascadia.QueryAll(node, selector)
+		nodes = append(nodes, result...)
+	}
+
+	return &SelectedNodes{
+		isFragment: false,
+		nodes:      nodes,
+	}
+}
 
 func (s *HTMLSender) transaction(c commands.TransactionCommand) {
+
+	nodes := make(map[uint]*SelectedNodes)
+	nodes[0] = &SelectedNodes{
+		isFragment: false,
+		nodes:      []*html.Node{s.document},
+	}
 
 	for _, command := range c.Transaction {
 
 		switch cmd := command.(type) {
 		case commands.SelectorSubCommand:
-			// TODO
+
+			parent := nodes[cmd.Parent]
+			if parent == nil {
+				continue
+			}
+
+			sel, err := s.selectorCache.Get(cmd.Selector, cmd.Args)
+			if err != nil {
+				continue
+			}
+
+			nodes[cmd.Id] = queryFirst(parent, sel)
+
 		case commands.SelectorAllSubCommand:
-			// TODO
+
+			parent := nodes[cmd.Parent]
+			if parent == nil {
+				continue
+			}
+
+			sel, err := s.selectorCache.Get(cmd.SelectorAll, cmd.Args)
+			if err != nil {
+				continue
+			}
+
+			nodes[cmd.Id] = queryAll(parent, sel)
+
 		case commands.FragmentSubCommand:
 			// TODO
 		case commands.ContentSubCommand:
