ba6c34c721cc4379bc7e3771868df3d28fe0ef63
Manuel Valls <me@manvalls.com>
2023-01-05 14:46:53 +0100

    Refactor CloneNode

--- util/clonenode.go
+++ util/clonenode.go
@@ -5,36 +5,23 @@ import (
 )
 
 func CloneNode(node *html.Node) *html.Node {
-	cache := map[*html.Node]*html.Node{}
-	cache[node.Parent] = nil
-	return cloneNode(node, cache)
-}
-
-func cloneNode(node *html.Node, cache map[*html.Node]*html.Node) *html.Node {
 	if node == nil {
 		return nil
 	}
 
-	if val, ok := cache[node]; ok {
-		return val
+	newNode := &html.Node{
+		Type:      node.Type,
+		DataAtom:  node.DataAtom,
+		Data:      node.Data,
+		Namespace: node.Namespace,
 	}
-
-	newNode := &html.Node{}
-	cache[node] = newNode
-
-	newNode.Parent = cloneNode(node.Parent, cache)
-	newNode.FirstChild = cloneNode(node.FirstChild, cache)
-	newNode.LastChild = cloneNode(node.LastChild, cache)
-	newNode.PrevSibling = cloneNode(node.PrevSibling, cache)
-	newNode.NextSibling = cloneNode(node.NextSibling, cache)
-
-	newNode.Type = node.Type
-	newNode.DataAtom = node.DataAtom
-	newNode.Data = node.Data
-	newNode.Namespace = node.Namespace
 
 	newNode.Attr = make([]html.Attribute, len(node.Attr))
 	copy(newNode.Attr, node.Attr)
+
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		newNode.AppendChild(CloneNode(c))
+	}
 
 	return newNode
 }
