e6c8e455787348be0ce05e07a555729891cebf9c
Manuel Valls <me@manvalls.com>
2023-02-25 18:50:49 +0100

    Improve locking of selector cache

--- selectorcache/selectorcache.go
+++ selectorcache/selectorcache.go
@@ -9,7 +9,7 @@ import (
 
 type SelectorCache struct {
 	cache map[string]cascadia.Sel
-	mux   sync.Mutex
+	mux   sync.RWMutex
 }
 
 func New() *SelectorCache {
@@ -19,22 +19,26 @@ func New() *SelectorCache {
 }
 
 func (s *SelectorCache) Get(selector string, args []interface{}) (cascadia.Sel, error) {
-	s.mux.Lock()
-	defer s.mux.Unlock()
-
 	if len(args) > 0 {
 		return cascadia.Parse(fmt.Sprintf(selector, args...))
 	}
 
+	s.mux.RLock()
+
 	if cached, ok := s.cache[selector]; ok {
+		s.mux.RUnlock()
 		return cached, nil
 	}
+
+	s.mux.RUnlock()
 
 	sel, err := cascadia.Parse(selector)
 	if err != nil {
 		return nil, err
 	}
 
+	s.mux.Lock()
+	defer s.mux.Unlock()
 	s.cache[selector] = sel
 	return sel, nil
 }
